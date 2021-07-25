82dd46512ea85b28ada13dd854c61ce0cd1ac2d3
Luis <luisjk266@gmail.com>
2021-07-25 21:49:40 +0100

    Flexing with headers and footers

--- src/main/java/dev/sllcoding/gameception/server/listeners/ServerListener.java
+++ src/main/java/dev/sllcoding/gameception/server/listeners/ServerListener.java
@@ -5,6 +5,7 @@ import com.github.christian162.annotations.Node;
 import com.github.christian162.interfaces.Listener;
 import dev.sllcoding.gameception.server.GameceptionServer;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.player.PlayerLoginEvent;
@@ -18,6 +19,11 @@ public class ServerListener implements Listener {
     public void onPlayerLogin(PlayerLoginEvent event) {
         event.setSpawningInstance(GameceptionServer.getInstance().getMainInstance());
         event.getPlayer().setRespawnPoint(new Pos(0, 40, 0));
+
+        event.getPlayer().sendPlayerListHeaderAndFooter(
+                MiniMessage.get().parse("                      \n                      <bold><gradient:#ff6c32:#ff76b6>Gameception</gradient></bold>                      \n                      "),
+                MiniMessage.get().parse("                      \n                      Bottom Text                      \n                      ")
+        );
     }
 
     @Listen
